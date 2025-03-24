34d3c862734abab44b288373f8bfe7164d90c538
Ticker <66017543+TickerDev@users.noreply.github.com>
2025-03-24 00:53:57 -0400

    Create server_config.lua

---
+++ server_config.lua
@@ -0,0 +1,12 @@
+Discord = {
+    DiscordServerID =  "", -- Your Discord Server ID
+
+    DiscordBotToken = "", -- Your Discord Bot Token
+
+    WebHook = "",  -- Your WebHook URL for logs
+
+    Image = "", -- Your Server Image URL  (96px x 96px)
+
+    color = 1432462
+}
+
